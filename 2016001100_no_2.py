ba48c44d6ea962b1d9f90d5d7f7273552e6e1d99
Ubuntu <ubuntu@ip-172-31-81-174.ec2.internal>
2020-10-27 00:29:29 +0000

    no_2_adicionado

---
+++ 2016001100_no_2.py
@@ -0,0 +1,34 @@
+import rospy
+from std_msgs.msg import String
+
+
+rospy.init_node('2016001100_no_2.py')
+
+matricula = 0
+
+def pega_valor(valor_publicado):
+    global matricula
+    matricula = valor_publicado.data
+
+
+sub = rospy.Subscriber('/topic', String, pega_valor)
+#soma = 0
+
+#for digito in str(matricula):
+#    soma += int(digito)
+    
+def timerCallBack(event):
+    soma = 0
+    for digito in str(matricula):
+        soma += int(digito)
+    msg = String()
+    msg.data = soma
+    pub.publish(msg)
+    
+    
+    
+pub = rospy.Publisher('/topic1', String, queue_size=1)
+timer = rospy.Timer(rospy.Duration(0.5), timerCallBack)
+
+
+rospy.spin()
